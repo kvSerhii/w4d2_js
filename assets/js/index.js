94801956151db8efab0dc6164002f8a8e9102c09
Serhii <kv.serhii@gmail.com>
2020-08-02 19:58:50 +0300

    add to js

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,10 +17,56 @@
 // let numTest1 = parseInt(prompt("Enter a number"));
 // let numTest2 = parseInt(prompt("Enter a number two"));
 
-let numTest1 = +prompt("Enter a number");
-let numTest2 = +prompt("Enter a number two");
+// let numTest1 = +prompt("Enter a number");
+// let numTest2 = +prompt("Enter a number two");
 
-let res = numTest1 + numTest2;
-console.log(res);
+// let res = numTest1 + numTest2;
+// console.log(res);
 
 //document.write(`<p>Some text</p>`);
+
+// if
+// let num = +prompt("Enter a number");
+
+// // if (num > 0 && num <= 15) {
+// if (0 > num <= 15) {
+//   console.log(`${num} it's first quoter`);
+// } else if (num > 15 && num <= 30) {
+//   console.log(`${num} it's second quoter`);
+// } else if (num > 30 && num <= 45) {
+//   console.log(`${num} it's third quoter`);
+// } else if (num > 45 && num <= 59) {
+//   console.log(`${num} it's fourth quoter`);
+// } else if (typeof num !== Number) {
+//   console.log("YOU ENTERED NOT A NUMBER");
+// } else {
+//   console.log("You entered wrong value");
+// }
+
+// cicles
+// let num = +prompt("enter a number");
+// let iteration = 0;
+// let iter = 0;
+// // console.log(`${num}`);
+
+// if (num < 100) {
+//   for (let i = 0; i < num; i++) {
+//     console.log("for #");
+//   }
+
+//   do {
+//     console.log("do..while #");
+//     iter++;
+//   } while (iter < num);
+
+//   while (iteration < num) {
+//     console.log("while #");
+//     iteration++;
+//   }
+// } else {
+//   console.log("It's not a number or your number too big! Try again!");
+//   num = +prompt("enter a number");
+// }
+
+//functions
+
