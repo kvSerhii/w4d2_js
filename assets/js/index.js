fdd09707006fec41a574947732a8e91eb0ae2b8b
Serhii <kv.serhii@gmail.com>
2020-08-02 17:24:11 +0300

    first js

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// let colorBlue = "blue";
+// console.log(colorBlue);
+
+// let string = `My favorite color is ${colorBlue}`;
+// console.log(string);
+//console.log(age);
+
+// let firstName = "Serhii";
+// let lastName = "Kononov";
+// string = `My name is ${firstName}, my last name is ${lastName}`;
+// console.log(string);
+
+// let firstName = prompt("What is your name?");
+// console.log(firstName);
+// let numTest1 = parseInt(prompt("Enter a number"));
+// let numTest2 = parseInt(prompt("Enter a number two"));
+
+let numTest1 = +prompt("Enter a number");
+let numTest2 = +prompt("Enter a number two");
+
+let res = numTest1 + numTest2;
+console.log(res);
+
+//document.write(`<p>Some text</p>`);
